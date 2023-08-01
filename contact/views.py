55fc77701741a6af2756c78130497f3df9ebd9b1
yannickferenczi <>
2023-08-01 09:00:33 +0000

    Update the ux design section of the README.md file

--- contact/views.py
+++ contact/views.py
@@ -15,4 +15,4 @@ def contact(request):
             return redirect("home")
     else:
         form = ContactForm()
-        return render(request, "contact.html", {"form": form})
+        return render(request, "contact/contact.html", {"form": form})
