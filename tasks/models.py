4256a81071ac8d55d8fdb698f1832219062b3a06
yannickferenczi <>
2023-08-02 05:55:22 +0000

    Add task model and its retrieve view

--- tasks/models.py
+++ tasks/models.py
@@ -1,3 +1,55 @@
 from django.db import models
+from django.urls import reverse
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Employee(models.Model):
+    first_name = models.CharField(max_length=250)
+    last_name = models.CharField(max_length=250)
+    active = models.BooleanField(default=True)
+
+    def __str__(self):
+        return self.full_name
+
+    @property
+    def full_name(self):
+        return f"{self.first_name} {self.last_name}"
+
+
+class Task(models.Model):
+    CATEGORIES = [
+        ("GARDENING", "Gardening"),
+        ("CLEANING", "Cleaning"),
+        ("GARDEN_PATCH", "Garden patch"),
+        ("SWIMMING_POOL", "Swimming pool"),
+        ("INSTALLATION_AND_REPAIR", "Renovation and repair"),
+        ("HOME_CARE", "Home care"),
+    ]
+    name = models.CharField(max_length=250, )
+    done = models.BooleanField(default=False)
+    owner = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        related_name="task_owner",
+    )
+    created_on = models.DateField(auto_now_add=True)
+    category = models.CharField(
+        max_length=50,
+        choices=CATEGORIES,
+        default="CLEANING",
+    )
+    repeat = models.BooleanField(default=False)
+    employee = models.ForeignKey(
+        Employee,
+        on_delete=models.PROTECT,
+        related_name="employees",
+        blank=True,
+        null=True,
+    )
+    comment = models.CharField(max_length=250, blank=True, null=True)
+
+    class Meta:
+        ordering = ["-created_on"]
+
+    def __str__(self):
+        return self.name
--- tasks/views.py
+++ tasks/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from django.views import generic
+from django.contrib.auth.mixins import LoginRequiredMixin
 
-# Create your views here.
+from .models import Task
+
+
+class TaskList(LoginRequiredMixin, generic.ListView):
+    model = Task
+    template_name = "tasks/dashboard.html"
+    paginated_by = 50
+
+    def get_queryset(self):
+        return Task.objects.filter(owner=self.request.user)
