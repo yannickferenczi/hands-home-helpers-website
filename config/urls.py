5224daee6162deba42dbd3bd6079de62bcfa071e
yannickferenczi <>
2023-08-03 08:24:54 +0000

    Add about page

--- config/urls.py
+++ config/urls.py
@@ -16,10 +16,11 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
-from .views import index
+from .views import index, about
 
 urlpatterns = [
     path('', index, name="home"),
+    path('about/', about, name="about"),
     path('administration_panel/', admin.site.urls),
     path('account/', include('allauth.urls')),
     path('contact/', include('contact.urls')),
