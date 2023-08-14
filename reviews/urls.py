9e5a2546e3a7acde2b0dbdfb079dc96838f18dbb
yannickferenczi <>
2023-08-14 20:29:16 +0000

    Fix style and responsiveness to display reviews

--- reviews/urls.py
+++ reviews/urls.py
@@ -3,7 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    # path('', views.reviews, name="reviews"),
     path(
         'add_review/',
         views.ReviewCreateView.as_view(),
--- reviews/views.py
+++ reviews/views.py
@@ -8,22 +8,6 @@ from django.contrib.messages.views import SuccessMessageMixin
 from .models import Review
 
 
-# def reviews(request):
-#     tasks = Task.objects.all()
-#     reviews = Review.objects.all()
-#     # filter(online=True).order_by(["created_on"])
-#     range_full_stars = {}
-#     range_empty_stars = {}
-#     for review in reviews:
-#         range_full_stars[review.id] = range(review.satisfaction)
-#         range_empty_stars[review.id] = range(5 - review.satisfaction)
-#     return render(
-#         request,
-#         "reviews/reviews_list.html",
-#         {"reviews": reviews, "tasks": tasks},
-#     )
-
-
 class ReviewCreateView(SuccessMessageMixin, LoginRequiredMixin, CreateView):
     model = Review
     template_name = "reviews/create_review.html"
